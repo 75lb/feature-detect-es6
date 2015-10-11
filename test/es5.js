8bfdd1a5cf33441ff10f52b1ec4d88b117787b0d
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-10-11 15:50:21 +0100

    added let and const tests

--- test/es5.js
+++ test/es5.js
@@ -5,3 +5,18 @@ test('.class()', function (t) {
   t.strictEqual(detect.class(), false)
   t.end()
 })
+
+test('.arrowFunction()', function (t) {
+  t.strictEqual(detect.arrowFunction(), false)
+  t.end()
+})
+
+test('.let()', function (t) {
+  t.strictEqual(detect.let(), false)
+  t.end()
+})
+
+test('.const()', function (t) {
+  t.strictEqual(detect.const(), false)
+  t.end()
+})
--- test/es6.js
+++ test/es6.js
@@ -5,3 +5,18 @@ test('.class()', function (t) {
   t.strictEqual(detect.class(), true)
   t.end()
 })
+
+test('.arrowFunction()', function (t) {
+  t.strictEqual(detect.arrowFunction(), true)
+  t.end()
+})
+
+test('.let()', function (t) {
+  t.strictEqual(detect.let(), true)
+  t.end()
+})
+
+test('.const()', function (t) {
+  t.strictEqual(detect.const(), true)
+  t.end()
+})
