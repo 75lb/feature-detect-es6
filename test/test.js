5a8860f1b8e180058c9f6667af6238462950237b
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-10-11 15:58:55 +0100

    test fix

--- test/test.js
+++ test/test.js
@@ -2,9 +2,8 @@ if (process.env.VERSION === 'node') {
   console.log('Running node tests')
   require('./es6-node')
 } else if (process.env.VERSION === 'iojs') {
-    console.log('Running iojs tests')
-    require('./es6-iojs')
-  } else {
+  console.log('Running iojs tests')
+  require('./es6-iojs')
 } else {
   console.log('Running ES5 tests')
   require('./es5')
