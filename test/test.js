a45d9d442e4c821359b6ea3dac58b66a9bead99e
Lloyd Brookes <lloyd.brookes@gmail.com>
2016-04-30 13:48:29 +0100

    upgrade license and travis

--- test/test.js
+++ test/test.js
@@ -1,22 +1,26 @@
 switch (process.env.VERSION) {
-  case '5.0':
-    console.log('Running 5.0 tests')
-    require('./es6-5.0')
+  case '6':
+    console.log('Running v6 tests')
+    require('./v6')
     break
-  case '4.1':
-    console.log('Running 4.1 tests')
-    require('./es6-4.1')
+  case '5':
+    console.log('Running v5 tests')
+    require('./v5')
+    break
+  case '4':
+    console.log('Running v4 tests')
+    require('./v4')
     break
   case 'iojs':
     console.log('Running iojs tests')
-    require('./es6-iojs')
+    require('./iojs')
     break
   case '0.12':
-    console.log('Running es5-0.12 tests')
-    require('./es5-0.12')
+    console.log('Running v0.12 tests')
+    require('./v0.12')
     break
   case '0.10':
-    console.log('Running es5-0.10 tests')
-    require('./es5-0.10')
+    console.log('Running v0.10 tests')
+    require('./v0.10')
     break
 }
